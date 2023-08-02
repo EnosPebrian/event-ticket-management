ccb1d865afae42914617b1441ff73c8ba9fc0aee
enospebrian1 <enospebrian1@gmail.com>
2023-08-02 21:23:56 +0700

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import Header from "./components/Header-navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import Eventdisplay from "./pages/event-display";
+import api from "./json-server/api";
+import { useState } from "react";
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [users, setUsers] = useState([]);
+  const fetchEvent = async () => {
+    const res_users = await api.get("/users");
+    const res_events = await api.get("/events");
+    setEvents([...res_users]);
+    setUsers([...res_events]);
+  };
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Eventdisplay />} />
-        <Rout path="/:eventname" element={<Eventdisplay />} />
+        <Route path="/:eventname" element={<Eventdisplay />} />
       </Routes>
     </>
   );
