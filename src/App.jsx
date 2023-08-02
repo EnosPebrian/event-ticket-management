180f2caacb6fc449ffc2a96a3bb30bf406e8dbc8
enospebrian1 <enospebrian1@gmail.com>
2023-08-02 22:39:00 +0700

    fetch ulang

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ import api from "./json-server/api";
 import { useEffect, useState } from "react";
 
 function App() {
+  //untuk set search, event-event, dan user-user
   const [search, setSearch] = useState([]);
   const [events, setEvents] = useState([]);
   const [users, setUsers] = useState([]);
+  //kalo berhasil login, pass datanya ke login
+  const [login, setLogin] = useState("");
   const fetchEvent = async () => {
-    const res_users = await api.get("/users");
-    const res_events = await api.get("/events");
-    setEvents([...res_users]);
-    setUsers([...res_events]);
+    try {
+      const res_users = await api.get("/users");
+      const res_events = await api.get("/events");
+      setEvents([...res_users]);
+      setUsers([...res_events]);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  //update Events dan Users pertama kali setelah document terload
   useEffect(() => {
     fetchEvent();
   }, []);
 
+  // update Events dan Users setelah nilai search diupdate
   useEffect(() => {
     fetchEvent();
   }, [search]);
