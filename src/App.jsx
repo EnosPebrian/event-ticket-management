25a4c71a415d1c4fc30cbee32cb5f1a272747632
enospebrian1 <enospebrian1@gmail.com>
2023-08-02 19:10:10 +0700

    base data

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header-navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Eventdisplay from "./pages/event-display";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Eventdisplay />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/event-display.jsx
@@ -0,0 +1,172 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
+// import ExampleCarouselImage from "components/ExampleCarouselImage";
+
+function Eventdisplay() {
+  return (
+    <>
+      <Container>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              src="https://api.yesplis.com/images/slider/8044935f5ad212cb8c3d74cb8d8fefdb158025d6.png.webp"
+              referrerPolicy="no-referrer"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="https://api.yesplis.com/images/slider/3baeaeeb8b92bdc3e02940d91dd4b68a204b7d52.png.webp"
+              referrerPolicy="no-referrer"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="https://api.yesplis.com/images/slider/521ccf964196249b8b09a63819dad8db5f0b4d5b.png.webp"
+              referrerPolicy="no-referrer"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+      <Container>
+        <Row className="mt-5">
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <img
+              src="https://w7.pngwing.com/pngs/620/280/png-transparent-one-circle-orange-white-number-1-orange-numbers-orange-circle.png"
+              width={"100%"}
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Eventdisplay;
