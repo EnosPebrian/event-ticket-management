a1a421cb9140b9f1e1c014545b5cf6e2a36aa87b
enospebrian <enospebrian1@gmail.com>
2023-08-23 17:21:23 +0700

    change API to localhost

--- src/json-server/api.jsx
+++ src/json-server/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://pale-goldenrod-wizard.glitch.me/" });
+const api = axios.create({ baseURL: "http://localhost:2000" });
 
 export default api;
