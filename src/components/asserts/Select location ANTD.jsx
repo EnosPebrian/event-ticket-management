d2fcf813c2e0316ec9cb667bbcb0e36f30f1e1b5
enospebrian <enospebrian1@gmail.com>
2023-08-29 08:33:03 +0700

    update sebelum berangkat

---
+++ src/components/asserts/Select location ANTD.jsx
@@ -0,0 +1,54 @@
+import { Select } from "antd";
+import { useEffect, useState } from "react";
+import api from "../../json-server/api";
+
+export const SelectLocationOpt = ({ formik }) => {
+  const [allLocation, setAllLocation] = useState([]);
+  const [locationName, setLocationName] = useState("");
+
+  const fetchLocation = async (location_name = "") => {
+    await api
+      .get(`/locations/q?location_name=${location_name}`)
+      .then((result) => setAllLocation(result.data));
+  };
+
+  const { Option } = Select;
+  function onChange(value) {
+    formik.values.location = value;
+  }
+  function onBlur() {
+    setLocationName("");
+  }
+  function onFocus() {}
+  function onSearch(val) {
+    setLocationName(val);
+  }
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+  useEffect(() => {
+    const debounce = setTimeout(() => fetchLocation(locationName), 500);
+    return () => clearTimeout(debounce);
+  }, [locationName]);
+
+  return (
+    <Select
+      showSearch
+      style={{ width: 200 }}
+      placeholder="Select a person"
+      optionFilterProp="children"
+      onChange={onChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onSearch={onSearch}
+      filterOption={(input, option) =>
+        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      }
+    >
+      {allLocation.map((val, index) => (
+        <Option value={val.id}>{val.location_name}</Option>
+      ))}
+    </Select>
+  );
+};
