be419f26b3a297d524ad991e13b352cb6ccad1ff
Andre Riyanto <andreriyantoo19@gmail.com>
2023-09-07 16:57:21 +0700

    update sikit

--- src/json-server/api.jsx
+++ src/json-server/api.jsx
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:2444" });
+const api = axios.create({
+  baseURL: "http://localhost:2444",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("auth")}`,
+  },
+});
 
 export default api;
